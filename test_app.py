15d894f956a72dc8d9e08a72ca1071944dd7f099
Vishal6283dk <vishal25goyal25@gmail.com>
2025-06-05 20:05:03 +0530

    Updated homepage response message

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     response=app.test_client().get("/")
 
     assert response.status_code==200
-    assert response.data== b"Hello, Testing the World!"
+    assert response.data== b"Hello Vishal, Welcome to this new World!"
